5c11b5d00a8826461a54edf91f078319b4e29f4f
Marcos Rocha <dev.marcost.rocha@gmail.com>
2025-11-11 09:09:46 -0300

    pequenos-ajustes

--- src/pages/perfil/components/rank.tsx
+++ src/pages/perfil/components/rank.tsx
@@ -33,7 +33,16 @@ export function Rank() {
 
       <div className="prf-rank-header">
         <div className="prf-container3">
-          <span>{t('ASTRONAUTAS')}</span>
+          <span><svg
+              xmlns="http://www.w3.org/2000/svg"
+              height="24px"
+              viewBox="0 -960 960 960"
+              width="24px"
+              fill="#FFFFFF"
+              style={{"marginRight":"10px"}}
+            >
+              <path d="M784-120 532-372q-30 24-69 38t-83 14q-109 0-184.5-75.5T120-580q0-109 75.5-184.5T380-840q109 0 184.5 75.5T640-580q0 44-14 83t-38 69l252 252-56 56ZM380-400q75 0 127.5-52.5T560-580q0-75-52.5-127.5T380-760q-75 0-127.5 52.5T200-580q0 75 52.5 127.5T380-400Z" />
+            </svg>{t('Pesquise por outros alunos')}</span>
           <div className="prf-pesquisa-usuario">
             <input
               name="pesquisa"
@@ -42,15 +51,6 @@ export function Rank() {
               value={pesquisa}
               onChange={(e) => setPesquisa(e.target.value)}
             />
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              height="24px"
-              viewBox="0 -960 960 960"
-              width="24px"
-              fill="#FFFFFF"
-            >
-              <path d="M784-120 532-372q-30 24-69 38t-83 14q-109 0-184.5-75.5T120-580q0-109 75.5-184.5T380-840q109 0 184.5 75.5T640-580q0 44-14 83t-38 69l252 252-56 56ZM380-400q75 0 127.5-52.5T560-580q0-75-52.5-127.5T380-760q-75 0-127.5 52.5T200-580q0 75 52.5 127.5T380-400Z" />
-            </svg>
           </div>
         </div>
       </div>
