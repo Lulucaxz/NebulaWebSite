01bf8e7f23230f014f07cb4b23f8960ddbc6f1b4
Marcos Rocha <dev.marcost.rocha@gmail.com>
2025-11-24 04:10:17 -0300

    usuario redirecionar url

--- src/pages/perfil/components/usuarioRank.tsx
+++ src/pages/perfil/components/usuarioRank.tsx
@@ -6,10 +6,22 @@ interface usuarioRankProps {
 }
 
 import { useTranslation } from 'react-i18next';
+import { useNavigate } from 'react-router-dom';
+
 const UsuarioRank = ({ fotoRank, nomeRank, pontosRank, posicaoRank }: usuarioRankProps) => {
   const { t } = useTranslation();
+  const navigate = useNavigate();
+
+  const handleUserClick = () => {
+    navigate('/perfil2');
+  };
+
   return (
-    <div className={parseInt(posicaoRank) % 2 == 0 ? "prf-usuarios-rank" : "prf-usuarios-rank prf-cor2"}>
+    <div
+      className={parseInt(posicaoRank) % 2 == 0 ? "prf-usuarios-rank" : "prf-usuarios-rank prf-cor2"}
+      onClick={handleUserClick}
+      style={{ cursor: "pointer" }}
+    >
       <div className="prf-container4" style={{position:"relative"}}>
         <div className="prf-foto-rank" style={{ backgroundImage: `url(${fotoRank})` }}></div>
         <div className="prf-nome-rank">
